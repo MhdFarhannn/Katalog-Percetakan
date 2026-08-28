57b855f738b3d83ac6dc814999cf590ad18d9182
MhdFarhannn <muhammadfarhan080926@gmail.com>
2026-08-28 09:15:05 +0700

    product

--- Models/Product.cs
+++ Models/Product.cs
@@ -3,10 +3,19 @@ namespace Katalog.Models
     public class Product
     {
         public int Id { get; set; }
+
         public int IdKategoriProduct { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string ImagePath { get; set; }
+
+        public int IdStatusProduct { get; set; }
+
+        public string Nama { get; set; } = string.Empty;
+
+        public string? Deskripsi { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        public decimal Harga { get; set; }
+
+        public string? BackgroundColor { get; set; }
     }
 }
---
+++ Services/ProductServices.cs
@@ -0,0 +1,15 @@
+using Katalog.Models;
+namespace Katalog.Services
+{
+    public class ProductServices
+    {
+        private readonly Database db;
+
+        public ProductServices(Database _db)
+        {
+            db = _db;
+        }
+
+        //MENAMBAHKAN PRODUCT
+    }
+}
